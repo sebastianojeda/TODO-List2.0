a9b0bed2fb982d0b554d88ca0406aaec60d3c3a9
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-21 12:19:13 -0700

    done with milestone 4

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
+//$mysqli->connect_error will kill the code and an erroir message will pop up
+if($mysqli->connect_error){
+	die('Connect Error(' . $mysqli->connect_errno . ')'
+		. $mysqli->connect_errno);
+}else{
+	echo "Connection Made";
+}
+$mysqli->close();
+
+
+
+
+?>
