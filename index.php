29387637d21a6861ab01a2228fd161abcdaf6c37
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-21 12:02:01 -0700

    initial commit. finished the first two milestones.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TODO-list2.0</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap"></div>
+</body>
+</html>
